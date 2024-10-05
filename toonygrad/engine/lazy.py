f8d01e8067265715aeed5698f5110714fc4f8b31
George Hotz <geohot@gmail.com>
2024-10-05 10:27:24 +0800

    lazy.py

--- toonygrad/engine/lazy.py
+++ toonygrad/engine/lazy.py
@@ -1,2 +1,11 @@
-from tinygrad.ops import UOp
-LazyBuffer = UOp
+from __future__ import annotations
+from typing import Tuple
+from tinygrad.ops import UOp, MetaOps
+from tinygrad.shape.symbolic import sint
+from tinygrad.dtype import DTypeLike, to_dtype
+
+class LazyBuffer(UOp):
+  @staticmethod
+  def metaop(op, shape:Tuple[sint,...], dtype:DTypeLike, device:str, arg=None, src:Tuple[LazyBuffer, ...]=(), enable_cache=False) -> LazyBuffer:
+    print("metaop", op)
+    pass
