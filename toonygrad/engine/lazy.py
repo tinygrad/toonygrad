c8f83a83ee6db9a3a1261f93fccd466d9a61afd5
George Hotz <geohot@gmail.com>
2024-10-05 10:34:25 +0800

    empty is bad

--- toonygrad/engine/lazy.py
+++ toonygrad/engine/lazy.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 from typing import Tuple
-from tinygrad.ops import UOp, MetaOps
-from tinygrad.shape.symbolic import sint
-from tinygrad.dtype import DTypeLike, to_dtype
+from toonygrad.ops import UOp, MetaOps
+from toonygrad.shape.symbolic import sint
+from toonygrad.dtype import DTypeLike, to_dtype
 
 class LazyBuffer(UOp):
   @staticmethod
   def metaop(op, shape:Tuple[sint,...], dtype:DTypeLike, device:str, arg=None, src:Tuple[LazyBuffer, ...]=(), enable_cache=False) -> LazyBuffer:
-    print("metaop", op)
+    if op is MetaOps.EMPTY: raise RuntimeError("why do we have this?")
+
     pass
